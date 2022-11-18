3b3e965103ab5b5fc1403a74af2bea679ad9fcee
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-11-18 09:38:09 +0100

    Classe Count: setter, getter, getMoney, addMoney

--- src/jana60/bank/Conto.java
+++ src/jana60/bank/Conto.java
@@ -1,5 +1,61 @@
 package jana60.bank;
 
 public class Conto {
+    private int numConto;
+    private String nomeProp;
+    private int saldo = 0;
+
+    public Conto(int numConto, String nomeProp){
+        setNumConto(numConto);
+        setNomeProp(nomeProp);
+    }
+
     
+
+    private void setNumConto(int numConto) {
+        this.numConto = numConto;
+    }
+
+
+
+    public void setNomeProp(String nomeProp) {
+        this.nomeProp = nomeProp;
+    }
+
+    
+
+    public int getNumConto() {
+        return numConto;
+    }
+
+
+
+    public String getNomeProp() {
+        return nomeProp;
+    }
+
+
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public String addMoney(int somma){
+        saldo += somma;
+        return "Il saldo aggiornato è: " + saldo;
+    }
+
+    public String getMoney(int prelievo){
+        if (prelievo <= saldo) {
+            saldo -= prelievo;
+            return "Sono stati prelevati: " + prelievo + "\nIl saldo aggiornato è: " + saldo;
+        }else{
+            return "La cifra da prelevare è maggiore del saldo depositato!";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Nome Proprietario: " + getNomeProp() + "\nNumero Conto: " + getNumConto() + "\nSaldo: " + getSaldo();
+    }
 }
--- src/jana60/bank/Main.java
+++ src/jana60/bank/Main.java
@@ -2,6 +2,10 @@ package jana60.bank;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Conto user = new Conto(123, "Alessio");
+        System.out.println(user);
+        System.out.println(user.addMoney(150));
+        System.out.println(user.getMoney(30));
+        System.out.println(user.getMoney(150));
     }
 }
